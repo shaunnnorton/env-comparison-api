c42185fb02c32aa2a72348bf83cb36702c5478a8
Shaun Norton <shaunnorton87@gmail.com>
2021-05-03 12:23:54 -0700

    Created User.get route

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,14 +7,27 @@ const router = Router()
 
 router.get("/", (req,res) => {
     User.estimatedDocumentCount().then((count) => {
-        let toSkip = Math.floor(Math.random() * count-req.query.amount)
-        console.log(req.query)
+        let toSkip = Math.abs(Math.floor(Math.random() * count-req.query.amount))
+        //console.log(req.query)
         return User.find({}).limit(Number(req.query.amount)).skip(toSkip).lean().populate()
     })
     .then(result => {
         res.send({Users:result})
     })
+    .catch(err => {
+        console.log(err)
+    })
 })
 
+router.get("/:username", (req,res) => {
+    User.findOne({'username':req.params.username}).lean().then(user => {
+        console.log(user)
+        //console.log(req.params.username)
+        res.send({"User":user})
+    })
+    .catch(err => {
+        console.log(err)
+    })
+})
 
 export default router
